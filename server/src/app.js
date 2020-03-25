34ca75b63e3bb3b65d1ea75b01f1933d873ce726
eveins <maxmost512@gmail.com>
2020-03-25 19:20:13 +0200

    commit

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,16 @@ const express = require( 'express' );
 
 const app = express();
 const expressWs = require( 'express-ws' )( app );
+const aWss = expressWs.getWss( '/' );
 
 app.use( express.json() );
 
 app.ws( '/', ( ws, req ) => {
   ws.on( 'message', ( msg ) => {
-    console.log( msg )
+    console.log( msg );
+    aWss.clients.forEach( ( client ) => {
+      client.send( msg );
+    } )
   } );
   console.log( req.testing )
 } );
